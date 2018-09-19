fdc31b40c424f51c9b6aa572bcd37c20e1c2a321
Saldin Ilya <saldin.iliya@gmail.com>
2018-09-19 17:37:29 +0300

    Feature/antd (#1)
    * Add css-modules&scss support
    
    * Fixes
    
    * Add antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 import * as React from 'react';
-import style from 'styled-components';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
-const Wrapper = style.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-`;
+import styles from './App.scss';
 
 const client = new ApolloClient({
   uri: 'https://nightly.stq.cloud/graphql',
@@ -16,7 +10,7 @@ const client = new ApolloClient({
 
 const App = () => (
   <ApolloProvider client={client}>
-    <Wrapper>hi</Wrapper>
+    <div className={styles.container}>hi</div>
   </ApolloProvider>
 );
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,6 @@ import * as ReactDOM from 'react-dom';
 
 import App from './App';
 
+import './styles.scss'; // tslint:disable-line
+
 ReactDOM.render(<App />, document.getElementById('root'));
