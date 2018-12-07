a9f936a9af6818ab18ae234ade4d645da498f025
Levenets Aleksey <a.levenec@storiqa.com>
2018-12-07 10:57:24 +0300

    add delete companies functionality

--- src/pages/DeliveryCompanies/queries.ts
+++ src/pages/DeliveryCompanies/queries.ts
@@ -12,4 +12,13 @@ const COMPANIES_LIST_QUERY = gql`
   }
 `;
 
-export { COMPANIES_LIST_QUERY };
+const DELETE_COMPANY_MUTATION = gql`
+  mutation DeleteCompanyMutation($id: Int!) {
+    deleteCompany(id: $id) {
+      id
+      rawId
+    }
+  }
+`;
+
+export { COMPANIES_LIST_QUERY, DELETE_COMPANY_MUTATION };
